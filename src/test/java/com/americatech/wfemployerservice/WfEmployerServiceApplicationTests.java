d6811badf274ebed003d242a2978d800ac90cb78
Md Ismail Hossain <ismail.hossain770m@gmail.com>
2025-11-20 11:42:25 +0600

    build basic structure

--- src/test/java/com/americatech/wfemployerservice/WfEmployerServiceApplicationTests.java
+++ src/test/java/com/americatech/wfemployerservice/WfEmployerServiceApplicationTests.java
@@ -1,13 +1,12 @@
 package com.americatech.wfemployerservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WfEmployerServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
